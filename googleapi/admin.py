23d407eb4b62f8f119d958ac9002a87f42ef29a9
rdboyett <rdboyett@gmail.com>
2013-11-02 14:06:51 -0500

    update

--- googleapi/admin.py
+++ googleapi/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from myproject.googleapi.models import *
 
-#admin.site.register(CredentialsModel)
 
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('dateTime', 'title')
+    search_fields = ('dateTime', 'title')
+    
 admin.site.register(UserInfo)
-admin.site.register(Project)
+admin.site.register(Project, ProjectAdmin)
 admin.site.register(FormInput)
 admin.site.register(BackImage)
+admin.site.register(TurnOn)
